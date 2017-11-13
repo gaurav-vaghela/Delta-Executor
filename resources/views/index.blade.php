c328ee27bfb5570d49d160136966806410b2e239
gaurav-vaghela <33055319+gaurav-vaghela@users.noreply.github.com>
2017-11-13 12:20:00 -0800

    home page update

---
+++ resources/views/index.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts.master')
+@section('content')
+<div class="banner">
+	 <div class="container">
+	 </div>
+</div>
+<!---->
+<div class="bride-grids">
+	 <div class="container">
+		 <div class="col-md-4 bride-grid">
+			 <div class="content-grid l-grids">
+				 <figure class="effect-bubba">
+						<img src="/front/images/b1.jpg" alt=""/>
+						<figcaption>
+							<h4></h4>
+							<p></p>																
+						</figcaption>			
+				 </figure>
+				 <div class="clearfix"></div>
+				 <h3>Wedding Dresses</h3>
+			 </div>
+			 <div class="content-grid l-grids">
+				 <figure class="effect-bubba">
+						<img src="/front/images/b2.jpg" alt=""/>
+						<figcaption>
+							<h4>abc </h4>
+							<p>xyza</p>																
+						</figcaption>			
+				 </figure>	
+				 <div class="clearfix"></div>
+				 <h3>BridalParty & Dresses</h3>
+			 </div>
+		 </div>
+		 <div class="col-md-4 bride-grid">
+				<div class="content-grid l-grids">
+						<img src="/front/images/brid.jpg" alt=""/>
+						
+				 <h3>Bridesmaid Dresses</h3>
+			 </div>
+		 </div>
+		 <div class="col-md-4 bride-grid">
+			 <div class="content-grid l-grids">
+				 <figure class="effect-bubba">
+						<img src="/front/images/b3.jpg" alt=""/>
+						<figcaption>
+							<h4>abc </h4>
+							<p>xyz</p>																
+						</figcaption>			
+				 </figure>	
+				 <div class="clearfix"></div>
+				 <h3>Wedding</h3>
+			 </div>
+			 <div class="content-grid l-grids">
+				 <figure class="effect-bubba">
+						<img src="/front/images/b4.jpg" alt=""/>
+						<figcaption>
+							<h4>abc</h4>
+							<p>abc</p>																
+						</figcaption>			
+				 </figure>
+					<div class="clearfix"></div>
+				 <h3>Most Beautiful</h3>
+			 </div>
+		 </div>
+		 <div class="clearfix"></div>
+	 </div>
+</div>
+<!---->
+@endsection
